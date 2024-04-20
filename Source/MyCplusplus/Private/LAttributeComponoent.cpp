32ae2eeea48ca2800cda84b6fa6a935686af1b1e
Zayywoo <167197012+Zayywoo@users.noreply.github.com>
2024-04-20 23:13:38 +0800

    第一次

--- Source/MyCplusplus/Private/LAttributeComponoent.cpp
+++ Source/MyCplusplus/Private/LAttributeComponoent.cpp
@@ -6,17 +6,8 @@
 // Sets default values for this component's properties
 ULAttributeComponoent::ULAttributeComponoent()
 {
-	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
-	// off to improve performance if you don't need them.
-	PrimaryComponentTick.bCanEverTick = true;
-
 	Health = 100;
 }
-
-
-
-
-
 
 bool ULAttributeComponoent::ApplyHealthChange(float Delta)
 {
